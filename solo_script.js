9d545f2fb04801a4812588a584d6e7f328784261
Kamiethenerd <Kamiethenerd@gmail.com>
2015-08-13 13:45:50 -0500

    v1

--- solo_script.js
+++ solo_script.js
@@ -1,59 +1,62 @@
-// ! ! !
-// Three Bugs
-//Only runs through the first array FIXED
-//Output pulling intial arrays FIXED
-//numbers aren't rounded FIXED
+ function Employee(name, num, salary, rating) {
+  this.firstName = name;
+  this.employeeNum = num;
+  this.salary = salary;
+  this.rating = rating; };
 
-var arrayAtticus = ["Atticus", "2405", "47000", 3];
-var arrayJem = ["Jem", "62347", "63500", 4];
-var arrayBoo = ["Boo", "11435", "54000", 3];
-var arrayScout = ["Scout", "6243", "74750", 5];
+var objAtticus = new Employee("Atticus", 2405, 47000, 3);
+var objJem = new Employee("Jem",  62347, 63500,  4);
+var objBoo = new Employee("Boo",  11435,  4000,  3);
+var objScout = new Employee("Scout", 6243,  74750,  50);
+console.log(objAtticus, objScout, objJem, objBoo);
 
-var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+var array = [objAtticus, objJem, objBoo, objScout];
 
 //Create variables used to write to the DOM
 var newEl, newText, position;
 //Capture the position of insertion into the DOM
 position = document.getElementById('content');
-var calcArray = [];
-//Loop the array, extracting each array and writing information to the DOM
-//Note that the information is not 'clean'
-for(var i = 0; i < array.length; i++){
-	//var employee = array[i]  ;// Something about this line breaks it? used "array" too much
- 	calculateSTI(array[i]);
-  newEl = document.createElement('li');
-  newText = document.createTextNode(calcArray[i]); // this line controls the output on the screen and is currently taking the starting info instead of the final calculated info
-	newEl.appendChild(newText);
-	position.appendChild(newEl);
-  
-  
-}
-  
-function calculateSTI(employee){
-  var newArray = [];
-  newArray[0] = employee[0];
-  
 
-  var employeeNumber = employee[1];
-  var baseSalary = employee[2];
-  var reviewScore = employee[3];
+console.log(array);
+for(var i = 0; i < array.length; i++){
+  array[i] = calculateSTI(array[i]);
+  newEl = document.createElement('li');
+  newText = document.createTextNode(array[i]);
+  newEl.appendChild(newText);
+  position.appendChild(newEl);
+  console.log (array[i]);
+}
+
+function calculateSTI(obj){
+  
+  function employeeTotal(name, bonusPerc, bonus, newSalary){
+    this.name = name;
+    this.bonusPerc = bonusPerc;
+    this.bonus = bonus;
+    this.newSalary = newSalary
+
+  }
+  var calc = new employeeTotal();
+  console.log(obj)
+
+  employeeTotal[0] = this.firstName;
+
+  var employeeNumber = this.employeeNum;
+  var baseSalary = this.salary;
+  var reviewScore = this.rating;
 
   var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
   if(bonus > 0.13){
     bonus = 0.13;
+    console.log(employeeTotal);
   }
 
-  newArray[1] = " " + (bonus * 100).toString() + "%";
-  newArray[2] = " " + Math.round(baseSalary * (1.0 + bonus)).toString(); // round the # FIXED
-  newArray[3] = " " + Math.round(baseSalary * bonus).toString(); //round the # FIXED
+  newArray[1] = " " + bonus + "%";
+  newArray[2] = " " + Math.round(baseSalary * (1.0 + bonus)).toString();
+  newArray[3] = " " + Math.round((baseSalary) * bonus).toString();
   console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
-  calcArray.push(newArray);
-  console.log(calcArray);
   return newArray;
-  
 }
-console.log(calcArray);
-
 
 function getBaseSTI(reviewScore){
   var basePercent;
@@ -74,7 +77,7 @@ function getBaseSTI(reviewScore){
       basePercent = 0.10;
       break;
   }
-  return basePercent ;
+  return basePercent;
 }
 
 function getYearAdjustment(employeeNumber){
